a5d69d993ce4d58e18c7e9dc0a4e0d6fc3cfbc65
linfuxiang <linfuxiang@yaochufa.com>
2018-06-05 18:26:00 +0800

    css压缩

--- webpack.prod.js
+++ webpack.prod.js
@@ -15,21 +15,6 @@ let extractCss = new ExtractTextPlugin('css/[name]-raw.[chunkhash].css')
 // Sass编译
 let extractScss = new ExtractTextPlugin('css/[name].[chunkhash].css')
 
-// 遍历所有HTML文件，根据这些文件进行打包
-let htmls = glob.sync('./view/**/*.html')
-let entries = {},
-    htmlPlugins = []
-htmls.forEach(function(filePath) {
-    let key = filePath.match(/.*(\S+)\.html$/)[1],
-        value = `./src/js/${key}.js`;
-    entries[key] = value
-    htmlPlugins.push(new HtmlWebpackPlugin({
-        filename: `view/${key}.html`, // 如：view/index.html
-        chunks: [key], // 需要添加的JS
-        template: filePath, // HTML模版
-    }))
-})
-
 let output = {}
 config.output = {
     filename: 'js/[name].[chunkhash].js',
@@ -42,13 +27,23 @@ config.module.rules = [{
     // use: ['style-loader', 'css-loader'],
     use: extractCss.extract({
         fallback: 'style-loader',
-        use: ['css-loader', 'postcss-loader'], // loader需要按照此先后顺序，否则报错
+        use: [{
+            loader: 'css-loader',
+            options: {
+                minimize: true,
+            }
+        }, 'postcss-loader'], // loader需要按照此先后顺序，否则报错
     }),
 }, {
     test: /\.scss$/,
     use: extractScss.extract({
         fallback: 'style-loader',
-        use: ['css-loader', 'postcss-loader', 'sass-loader'], // loader需要按照此先后顺序，否则报错
+        use: [{
+            loader: 'css-loader',
+            options: {
+                minimize: true,
+            }
+        }, 'postcss-loader', 'sass-loader'], // loader需要按照此先后顺序，否则报错
     }),
 }, {
     test: /\.(png|jpeg|jpg|gif)$/,
